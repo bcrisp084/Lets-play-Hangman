c0f8207568a7441ce1050ca8c35df3fc33fb3b36
Brian Crisp <bcrisp084@yahoo.com>
2021-09-06 18:22:39 -0400

    refactored the function for creating the buttons and added more divs to the page for storing some game info

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-// create array of random words
-// iterate through the array to select a random word for the game
-// set up array for pushing users answers to the game
-// prompt user for a guess
-// check the guess against the letters in the current word.
-// if lette is correct show some validation
-// if choice is wrong push wrong choice to array and game is one step closer to being over
-// 
 
-window.onload = function () {
-    alert('please select a letter to begin the game')
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const gamePhrases = {
+    phrase1: {
+        phrase: 'programming',
+        category: 'technology',
+        hint: 'the skill required to create this game',
+    },
 
+    phrase2: {
+        phrase: 'batman',
+        category: 'super heroes',
+        hint: 'his ears are pointy',
+    },
 
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    phrase3: {
+        phrase: 'barber',
+        category: 'grooming',
+        hint: 'i shave every day but my beard stays the same',
+    },
 
-    const buttons = function () {
-        manyButtons = document.getElementById("button");
-        letters = document.createElement("ul")
-
-        for (let i = 0; i < alphabet.length; i++) {
-            letters.id = "alphabet";
-            list = document.createElement("li");
-            list.id = "letter";
-            list.innerHTML = alphabet[i];
-            manyButtons.appendChild(letters);
-            letters.appendChild(list);
-
-        }
+    phrase4: {
+        phrase: 'darkness',
+        category: 'interesting facts',
+        hint: 'the more there is the less you see'
     }
-    buttons()
 }
+
+$(document).ready(function () {
+
+    for (let i = 0; i < alphabet.length; i++) {
+        const element = alphabet[i]
+        const letterBtn = $('<button>')
+        letterBtn.addClass("letter-button letter letter-button-color");
+        letterBtn.attr("data-letter", element)
+        letterBtn.text(element)
+        $('#alphabet').append(letterBtn)
+    }
+
+    function loadGame() {
+
+    }
+
+})
+
+
