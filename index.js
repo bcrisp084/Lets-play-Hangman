9570bfeb36b577029e98ce205055a65f5b07fb91
Brian Crisp <bcrisp084@yahoo.com>
2021-09-06 21:41:29 -0400

    created functions to build a game character to play with on the html canvas

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 const gamePhrases = {
     phrase1: {
         phrase: 'programming',
-        category: 'technology',
-        hint: 'the skill required to create this game',
+        category: ' CATEGORY : technology',
+        hint: ' HINT : the skill required to create this game',
     },
 
     phrase2: {
         phrase: 'batman',
-        category: 'super heroes',
-        hint: 'his ears are pointy',
+        category: ' CATEGORY : super heroes',
+        hint: ' HINT : his ears are pointy',
     },
 
     phrase3: {
         phrase: 'barber',
-        category: 'grooming',
-        hint: 'i shave every day but my beard stays the same',
+        category: ' CATEGORY : grooming',
+        hint: ' HINT : i shave every day but my beard stays the same',
     },
 
     phrase4: {
         phrase: 'darkness',
-        category: 'interesting facts',
-        hint: 'the more there is the less you see'
+        category: ' CATEGORY : interesting facts',
+        hint: ' HINT : the more there is the less you see'
     }
 }
 
@@ -39,9 +39,66 @@ $(document).ready(function () {
     }
 
     function loadGame() {
+        var num = Math.floor(Math.random() * 4) + 1;
+        $('.category').text(gamePhrases.phrase1.category)
+        $('.hint').text(gamePhrases.phrase1.hint)
 
     }
+    loadGame()
 
+
+    function drawBody() {
+        const c = document.getElementById("myCanvas")
+        const torso = c.getContext('2d')
+        torso.moveTo(300, 80);
+        torso.lineTo(300, 200);
+        torso.stroke();
+        torso.lineWidth = 7;
+        torso.lineCap = 'round'
+    }
+
+    function leftLeg() {
+        const c = document.getElementById('myCanvas')
+        const leftLeg = c.getContext('2d')
+        leftLeg.moveTo(300, 200)
+        leftLeg.lineTo(250, 250)
+        leftLeg.stroke()
+    }
+    function rightLeg() {
+        const c = document.getElementById('myCanvas')
+        const rightLeg = c.getContext('2d')
+        rightLeg.moveTo(300, 200)
+        rightLeg.lineTo(350, 250)
+        rightLeg.stroke()
+    }
+    function rightArm() {
+        const c = document.getElementById('myCanvas')
+        const rightArm = c.getContext('2d')
+        rightArm.moveTo(300, 110)
+        rightArm.lineTo(350, 150)
+        rightArm.stroke()
+    }
+    function leftArm() {
+        const c = document.getElementById('myCanvas')
+        const leftArm = c.getContext('2d')
+        leftArm.moveTo(300, 110)
+        leftArm.lineTo(250, 150)
+        leftArm.stroke()
+    }
+    function head() {
+        const c = document.getElementById('myCanvas')
+        const head = c.getContext('2d')
+        head.beginPath()
+        head.arc(300, 40, 35, 0, 2 * Math.PI)
+        head.stroke()
+    }
+
+    drawBody()
+    leftLeg()
+    rightLeg()
+    rightArm()
+    leftArm()
+    head()
 })
 
 
