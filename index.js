19a0553bfefde1617ca8189acf28f587a1425beb
Brian Crisp <bcrisp084@yahoo.com>
2021-09-06 23:27:07 -0400

    fixed a bug for the lines on the canvas not rendering correctly

--- index.js
+++ index.js
@@ -50,11 +50,11 @@ $(document).ready(function () {
         const letterClicked = ($(this).attr('data-letter'))
         if (letterClicked === 'f') {
             drawBody()
-            leftLeg()
-            rightLeg()
-            rightArm()
-            leftArm()
-            head()
+            // leftLeg()
+            // rightLeg()
+            // rightArm()
+            // leftArm()
+            // head()
 
         }
 
@@ -65,65 +65,64 @@ $(document).ready(function () {
     function drawBody() {
         const c = document.getElementById("myCanvas")
         const torso = c.getContext('2d')
+        torso.lineWidth = 7;
+        torso.lineCap = 'round'
         torso.moveTo(300, 80);
         torso.lineTo(300, 200);
         torso.stroke();
-        torso.lineWidth = 7;
-        torso.lineCap = 'round'
+
     }
 
     function leftLeg() {
         const c = document.getElementById('myCanvas')
         const leftLeg = c.getContext('2d')
+        leftLeg.lineWidth = 7;
+        leftLeg.lineCap = 'round'
         leftLeg.moveTo(300, 200)
         leftLeg.lineTo(250, 250)
         leftLeg.stroke()
-        leftLeg.lineWidth = 7;
-        leftLeg.lineCap = 'round'
+
     }
     function rightLeg() {
         const c = document.getElementById('myCanvas')
         const rightLeg = c.getContext('2d')
+        rightLeg.lineWidth = 7;
+        rightLeg.lineCap = 'round'
         rightLeg.moveTo(300, 200)
         rightLeg.lineTo(350, 250)
         rightLeg.stroke()
-        rightLeg.lineWidth = 7;
-        rightLeg.lineCap = 'round'
+
     }
     function rightArm() {
         const c = document.getElementById('myCanvas')
         const rightArm = c.getContext('2d')
+        rightArm.lineWidth = 7;
+        rightArm.lineCap = 'round'
         rightArm.moveTo(300, 110)
         rightArm.lineTo(350, 150)
         rightArm.stroke()
-        rightArm.lineWidth = 7;
-        rightArm.lineCap = 'round'
+
     }
     function leftArm() {
         const c = document.getElementById('myCanvas')
         const leftArm = c.getContext('2d')
+        leftArm.lineWidth = 7;
+        leftArm.lineCap = 'round'
         leftArm.moveTo(300, 110)
         leftArm.lineTo(250, 150)
         leftArm.stroke()
-        leftArm.lineWidth = 7;
-        leftArm.lineCap = 'round'
+
     }
     function head() {
         const c = document.getElementById('myCanvas')
         const head = c.getContext('2d')
+        head.lineWidth = 7;
+        head.lineCap = 'round'
         head.beginPath()
         head.arc(300, 40, 35, 0, 2 * Math.PI)
         head.stroke()
-        head.lineWidth = 7;
-        head.lineCap = 'round'
-    }
 
-    // drawBody()
-    // leftLeg()
-    // rightLeg()
-    // rightArm()
-    // leftArm()
-    // head()
+    }
 })
 
 
