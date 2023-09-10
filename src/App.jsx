b38613d913de9879806fe172d596043bbdd01ab1
Brian Crisp <bcrisp084@yahoo.com>
2023-09-09 22:50:46 -0400

    Removed default styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Box } from "@mui/material";
+
 function App() {
   return (
     <>
       <h1>Lets play Hangman!</h1>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box sx={{ width: "50%" }}>
+          <h2>Game</h2>
+          <p>Game goes here</p>
+        </Box>
+        <Box sx={{ width: "50%" }}>
+          <h2>Score</h2>
+          <p>Score goes here</p>
+        </Box>
+      </Box>
     </>
   );
 }
