bf57fff6b5ad71b1e40d52204833f26e92f5e041
Brian Crisp <bcrisp084@yahoo.com>
2023-09-09 22:36:44 -0400

    Added word categories

---
+++ src/utils/categories.js
@@ -0,0 +1,103 @@
+export const movies = [
+  "avatar",
+  "inception",
+  "titanic",
+  "starwars",
+  "jurassicpark",
+  "jaws",
+  "matrix",
+  "harrypotter",
+  "forrestgump",
+  "gladiator",
+];
+
+export const countries = [
+  "usa",
+  "canada",
+  "france",
+  "germany",
+  "japan",
+  "australia",
+  "brazil",
+  "india",
+  "china",
+  "russia",
+];
+
+export const animals = [
+  "lion",
+  "elephant",
+  "tiger",
+  "giraffe",
+  "zebra",
+  "kangaroo",
+  "penguin",
+  "dolphin",
+  "cheetah",
+  "panda",
+];
+
+export const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "strawberry",
+  "grape",
+  "watermelon",
+  "mango",
+  "pineapple",
+  "kiwi",
+  "peach",
+];
+
+export const programmingLanguages = [
+  "javascript",
+  "python",
+  "java",
+  "csharp",
+  "ruby",
+  "php",
+  "typescript",
+  "swift",
+  "go",
+  "rust",
+];
+
+export const sports = [
+  "soccer",
+  "basketball",
+  "tennis",
+  "baseball",
+  "golf",
+  "swimming",
+  "volleyball",
+  "cricket",
+  "rugby",
+  "hockey",
+];
+
+export const cars = [
+  "toyota",
+  "honda",
+  "ford",
+  "chevrolet",
+  "bmw",
+  "mercedes",
+  "nissan",
+  "volkswagen",
+  "audi",
+  "hyundai",
+];
+
+export const music = [
+  "rock",
+  "pop",
+  "hiphop",
+  "jazz",
+  "country",
+  "classical",
+  "reggae",
+  "blues",
+  "electronic",
+  "rap",
+];
